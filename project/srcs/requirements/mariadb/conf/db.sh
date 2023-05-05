80b16d2e92d64eae2f6950e4bacfbfab83f0ff03
Oumaima Fisaoui <oufisaou@e2r9p14.1337.ma>
2023-05-05 16:56:31 +0100

    e

--- project/srcs/requirements/mariadb/conf/db.sh
+++ project/srcs/requirements/mariadb/conf/db.sh
@@ -1,12 +1,14 @@
 #!bin/sh
-if [-d /var/lib/mysql/wordpress]; then
-    cd /etc/mysql/mariadb.conf.d/;
-    sed -i "s/127.0.0.1/0.0.0.0/g" 50-server.cnf;
+cd /etc/mysql/mariadb.conf.d/;
+sed -i "s/127.0.0.1/0.0.0.0/g" 50-server.cnf;
+if [ ! -d /var/lib/mysql/wordpress ]; then
     service mysql start;
-    mysql -u root -e "CREATE DATABASE wordpress IF NOT EXISTS wordpress;"
-    mysql -u root -r "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
+    #change the variables to the .env GLOBAL variables
+    mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+    mysql -u root -r "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
+    mysql -u root -r "CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';"
     mysql -u root -r "GRANT ALL PRIVILEGES ON wordpress TO 'admin'@'localhost';"
     mysql -u root -e "FLUSH PRIVILEGES;"
     mysqladmin shutdown;
-    tail -f;
 fi
+tail -f
