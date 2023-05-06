5946ba0c3c0302c38d05bd9bc6ae268a1b4a6277
Oumaima Fisaoui <oufisaou@e2r7p14.1337.ma>
2023-05-06 13:14:46 +0100

    done docker-compose

--- project/srcs/requirements/mariadb/conf/db.sh
+++ project/srcs/requirements/mariadb/conf/db.sh
@@ -5,8 +5,8 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
     service mysql start;
     #change the variables to the .env GLOBAL variables
     mysql -u root -e "CREATE DATABASE ${DB_NAME};"
-    mysql -u root -r "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
-    mysql -u root -r "CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';"
+    mysql -u root -r "CREATE USER '${BB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+    mysql -u root -r "CREATE USER '${DB_ROOT}'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
     mysql -u root -r "GRANT ALL PRIVILEGES ON wordpress TO 'admin'@'localhost';"
     mysql -u root -e "FLUSH PRIVILEGES;"
     mysqladmin shutdown;
