196794b86055dcae6b4f610e5305590264edd7f3
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-05-10 14:27:05 +0100

    E

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,8 +1,5 @@
-#!/bin/sh]
-
-
+#!/bin/sh
 # sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
-
 config_file="/var/www/html/config.php"
 
 if [ ! -d "$config_file" ]; then
@@ -12,25 +9,20 @@ if [ ! -d "$config_file" ]; then
             tar -xf latest.tar.gz --strip-components=1 && \
             rm -f latest.tar.gz && \
         
-        echo "<?php"
-        echo "define( 'DB_NAME', '${DB_NAME}' );"
-        echo "define( 'DB_USER', '${DB_USER}' );"
-        echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
-        echo "define( 'DB_HOST', 'mariadb' );"
-        echo "define( 'DB_CHARSET', 'utf8' );"
-        echo "define( 'DB_COLLATE', '' );"
-        echo "define('FS_METHOD','direct');"
-        echo "\$table_prefix = 'wp_';"
-        echo "define( 'WP_DEBUG', false );"
-        echo "if ( ! defined( 'ABSPATH' ) ) {"
-        echo "define( 'ABSPATH', __DIR__ . '/' );"
-        echo "}"
-        # echo "define( 'WP_REDIS_HOST', 'redis' );"
-        # echo "define( 'WP_REDIS_PORT', 6379 );"
-        # echo "define( 'WP_REDIS_TIMEOUT', 1 );"
-        # echo "define( 'WP_REDIS_READ_TIMEOUT', 1 );"
-        # echo "define( 'WP_REDIS_DATABASE', 0 );"
-        echo "require_once ABSPATH . 'wp-settings.php';"
+        # echo "<?php"
+        # echo "define( 'DB_NAME', '${DB_NAME}' );"
+        # echo "define( 'DB_USER', '${DB_USER}' );"
+        # echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
+        # echo "define( 'DB_HOST', 'mariadb' );"
+        # echo "define( 'DB_CHARSET', 'utf8' );"
+        # echo "define( 'DB_COLLATE', '' );"
+        # echo "define('FS_METHOD','direct');"
+        # echo "\$table_prefix = 'wp_';"
+        # echo "define( 'WP_DEBUG', false );"
+        # echo "if ( ! defined( 'ABSPATH' ) ) {"
+        # echo "define( 'ABSPATH', __DIR__ . '/' );"
+        # echo "}"
+        # echo "require_once ABSPATH . 'wp-settings.php';"
     } > "$config_file"
 
     mkdir /run/php && chown root:root /run/php && chmod 755 /run/php
