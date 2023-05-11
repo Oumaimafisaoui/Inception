7fdecfa0ef544624e399ff6691fd1dd235cd1a68
Oumaima Fisaoui <oufisaou@e2r7p14.1337.ma>
2023-05-11 16:53:27 +0100

    E

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
-# sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
-config_file="/var/www/html/config.php"
+
+config_file="/var/www/html"
 
 if [ ! -d "$config_file" ]; then
-    {
-        mkdir -p /var/www/html && cd /var/www/html
-        wget -q http://wordpress.org/latest.tar.gz && \
-            tar -xf latest.tar.gz --strip-components=1 && \
-            rm -f latest.tar.gz && \
-        
-        # echo "<?php"
-        # echo "define( 'DB_NAME', '${DB_NAME}' );"
-        # echo "define( 'DB_USER', '${DB_USER}' );"
-        # echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
-        # echo "define( 'DB_HOST', 'mariadb' );"
-        # echo "define( 'DB_CHARSET', 'utf8' );"
-        # echo "define( 'DB_COLLATE', '' );"
-        # echo "define('FS_METHOD','direct');"
-        # echo "\$table_prefix = 'wp_';"
-        # echo "define( 'WP_DEBUG', false );"
-        # echo "if ( ! defined( 'ABSPATH' ) ) {"
-        # echo "define( 'ABSPATH', __DIR__ . '/' );"
-        # echo "}"
-        # echo "require_once ABSPATH . 'wp-settings.php';"
-    } > "$config_file"
+{
+    mkdir -p /var/www/html && cat w
+    wget -q http://wordpress.org/latest.tar.gz && \
+        tar -xf latest.tar.gz --strip-components=1 && \
+        rm -f latest.tar.gz
+    
+    # echo "<?php"
+    # echo "define( 'DB_NAME', '${DB_NAME}' );"
+    # echo "define( 'DB_USER', '${DB_USER}' );"
+    # echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
+    # echo "define( 'DB_HOST', 'mariadb' );"
+    # echo "define( 'DB_CHARSET', 'utf8' );"
+    # echo "define( 'DB_COLLATE', '' );"
+    # echo "define('FS_METHOD','direct');"
+    # echo "\$table_prefix = 'wp_';"
+    # echo "define( 'WP_DEBUG', false );"
+    # echo "if ( ! defined( 'ABSPATH' ) ) {"
+    # echo "define( 'ABSPATH', __DIR__ . '/' );"
+    # echo "}"
+# } > "$config_file"
 
     mkdir /run/php && chown root:root /run/php && chmod 755 /run/php
-   
+}
 fi
-/usr/sbin/php-fpm7.3 -F
+tail -f
