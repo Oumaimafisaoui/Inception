d022d8e9546106ab324456e6b7f8f8025fdca23f
Oumaima Fisaoui <oufisaou@e2r9p14.1337.ma>
2023-05-23 15:27:19 +0100

    E

--- project/srcs/requirements/mariadb/conf/db.sh
+++ project/srcs/requirements/mariadb/conf/db.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-cd /etc/mysql/mariadb.conf.d/;
-sed -i "s/127.0.0.1/0.0.0.0/g" 50-server.cnf;
-if [ ! -d /var/lib/mysql/wordpress ]; then
-    service mysql start;
-    #change the variables to the .env GLOBAL variables
+cd /etc/mysql/mariadb.conf.d/
+sed -i "s/127.0.0.1/0.0.0.0/g" 50-server.cnf
+
+if [ ! -d /var/lib/mysql/$DB_NAME ]; then
+    service mysql start
+    sleep 5
+    # Change the variables to the actual values or ensure they are correctly set in your environment
     mysql -u root -e "CREATE DATABASE ${DB_NAME};"
-    mysql -u root -r "CREATE USER '${BB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
-    mysql -u root -r "CREATE USER '${DB_ROOT}'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
-    mysql -u root -r "GRANT ALL PRIVILEGES ON wordpress TO 'admin'@'localhost';"
-    mysql -u root -e "FLUSH PRIVILEGES;"
-    mysqladmin shutdown;
+    mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PS_ROOT}';"
+    mysql -u root -e "FLUSH PRIVILEGES;" -p${PS_ROOT}
+
+    mysqladmin shutdown -p${PS_ROOT}
 fi
-tail -f
+mysqld_safe --console
--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-config_file="/var/www/html"
+config_file="/var/www/html/wp-config.php"
 
 if [ ! -d "$config_file" ]; then
 {
-    mkdir -p /var/www/html && cat w
+    mkdir -p /var/www/html 
+    cd /var/www/html
     wget -q http://wordpress.org/latest.tar.gz && \
         tar -xf latest.tar.gz --strip-components=1 && \
         rm -f latest.tar.gz
     
-    wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --dbcharset=utf8 --dbcollate= --force
+    wp-cli core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --dbcharset=utf8 --dbcollate= --force --allow-root
     
-    wp core install --url=http://localhost --title="Welcome to My Site" --admin_user=admin --admin_password=adminpass --admin_email=admin@example.com --skip-email
+    wp-cli core install --url=http://localhost --title="Welcome to My Site" --admin_user=admin --admin_password=adminpass --admin_email=admin@example.com --skip-email  --allow-root
     
-    wp user create testuser test@example.com --role=author --user_pass=testpass
+    wp-cli user create testuser test@example.com --role=author --user_pass=testpass  --allow-root
     
-    wp post create --post_type=page --post_title="Welcome" --post_content="This is a welcome page." --post_status=publish
+    wp-cli post create --post_type=page --post_title="Welcome" --post_content="This is a welcome page." --post_status=publish  --allow-root
     
-    wp option update show_on_front 'page'
-    wp option update page_on_front 2
+    wp-cli option update show_on_front 'page'
+    wp-cli option update page_on_front 2
     
-    wp rewrite structure '/%postname%/'
+    wp-cli rewrite structure '/%postname%/'
     
     chown -R www-data:www-data /var/www/html
     chmod -R 755 /var/www/html
 }
 fi
+php7.3-fpm -F 
