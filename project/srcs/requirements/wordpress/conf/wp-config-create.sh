f8438334fbe8a501dff140c8effb2a16abe824bb
Oumaima Fisaoui <oufisaou@e2r9p14.1337.ma>
2023-05-23 11:17:17 +0100

    E

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -9,22 +9,20 @@ if [ ! -d "$config_file" ]; then
         tar -xf latest.tar.gz --strip-components=1 && \
         rm -f latest.tar.gz
     
-    # echo "<?php"
-    # echo "define( 'DB_NAME', '${DB_NAME}' );"
-    # echo "define( 'DB_USER', '${DB_USER}' );"
-    # echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
-    # echo "define( 'DB_HOST', 'mariadb' );"
-    # echo "define( 'DB_CHARSET', 'utf8' );"
-    # echo "define( 'DB_COLLATE', '' );"
-    # echo "define('FS_METHOD','direct');"
-    # echo "\$table_prefix = 'wp_';"
-    # echo "define( 'WP_DEBUG', false );"
-    # echo "if ( ! defined( 'ABSPATH' ) ) {"
-    # echo "define( 'ABSPATH', __DIR__ . '/' );"
-    # echo "}"
-# } > "$config_file"
-
-    mkdir /run/php && chown root:root /run/php && chmod 755 /run/php
+    wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --dbcharset=utf8 --dbcollate= --force
+    
+    wp core install --url=http://localhost --title="Welcome to My Site" --admin_user=admin --admin_password=adminpass --admin_email=admin@example.com --skip-email
+    
+    wp user create testuser test@example.com --role=author --user_pass=testpass
+    
+    wp post create --post_type=page --post_title="Welcome" --post_content="This is a welcome page." --post_status=publish
+    
+    wp option update show_on_front 'page'
+    wp option update page_on_front 2
+    
+    wp rewrite structure '/%postname%/'
+    
+    chown -R www-data:www-data /var/www/html
+    chmod -R 755 /var/www/html
 }
 fi
-tail -f
