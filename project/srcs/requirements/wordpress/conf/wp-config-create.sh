bddb2d3cd5ad0440d5e979aba8a1f21beb173297
Oumaima Fisaoui <oufisaou@e2r9p15.1337.ma>
2023-05-24 13:56:03 +0100

    E

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+    mkdir -p /run/php/;
+	touch /run/php/php7.3-fpm.pid; #Store PID files for PHP processes managed by the PHP-FPM
+	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 config_file="/var/www/html/wp-config.php"
 
 if [ ! -d "$config_file" ]; then
@@ -12,12 +14,16 @@ if [ ! -d "$config_file" ]; then
     mv wp-cli.phar /usr/local/bin/wp
 
     wp core download --locale=en_US --allow-root
+    mv wp-config-sample.php wp-config.php
+    sed -i "s/database_name_here/${DB_NAME}/g" wp-config.php
+    sed -i "s/username_here/${DB_USER}/g" wp-config.php
+    sed -i "s/password_here/${DB_PASS}/g" wp-config.php
+    sed -i "s/localhost/mariadb/g" wp-config.php
+
     wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --locale=en_US --allow-root
     
     wp core install --url=https://localhost --title="Welcome to My Site" --admin_user=admin --admin_password=adminpass --admin_email=admin@example.com --skip-email --allow-root 
     
-    mkdir -p /run/php
-    chown -R www-data:www-data /var/www/html
 }
 fi
-php7.3-fpm -F -R
+exec "$@"
